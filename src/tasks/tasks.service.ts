79e6bd6ff5c079c4ee1ea4141aa476bf700522e0
GenDev0 <dev.gen.info.0@gmail.com>
2023-02-24 15:27:18 +0100

    add getTaskById to S-C

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -7,10 +7,18 @@ import { CreateTaskDto } from './dto/create-task.dto';
 export class TasksService {
   private tasks: Task[] = [];
 
+  // Get All Tasks
   getAllTasks(): Task[] {
     return this.tasks;
   }
 
+  // Get a task by ID
+  getTaskById(id: string): Task {
+    const task = this.tasks.find((task) => task.id === id);
+    return task;
+  }
+
+  // Create a new Task
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
     const task: Task = {

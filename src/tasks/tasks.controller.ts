a576ccef327529b168f7ceb9ef18c89a7df11029
GenDev0 <dev.gen.info.0@gmail.com>
2023-02-24 11:23:56 +0100

    add a Task model

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksSercive: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasksSercive.getAllTasks();
   }
 }

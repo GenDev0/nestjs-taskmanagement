45919aee3699ad9911d123781ceb6443d1441763
GenDev0 <dev.gen.info.0@gmail.com>
2023-02-25 15:11:46 +0100

    add Config file for typeorm

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mariadb',
+  host: 'localhost',
+  port: 3307,
+  username: 'root',
+  password: '',
+  database: 'managetasks',
+  entities: [__dirname + '../**/*.entity.ts'],
+  synchronize: true,
+};

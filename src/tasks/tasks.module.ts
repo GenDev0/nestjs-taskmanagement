3db12d3262fe27339c158003d0a4569bf50b1877
GenDev0 <dev.gen.info.0@gmail.com>
2023-02-26 16:57:25 +0100

    import authModule in taskModule

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,3 +1,4 @@
+import { AuthModule } from './../auth/auth.module';
 import { Taskrepository } from './task.repository';
 import { Task } from './task.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,7 +7,7 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService, Taskrepository],
   // exports : [TasksService , Taskrepository], // add this only if you use service and/or custom repo within another module/service

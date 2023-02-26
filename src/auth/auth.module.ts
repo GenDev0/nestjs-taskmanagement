af287c3f514f60803b1659ebba33776a5994e125
GenDev0 <dev.gen.info.0@gmail.com>
2023-02-26 12:20:44 +0100

    add a user Repository and Import/Export it in authModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, UserRepository],
+  //exports : [AuthService,UserRepository], // add this only if you use service and/or custom repo within another module/service
 })
 export class AuthModule {}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,18 @@
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+export class UserRepository extends Repository<User> {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {
+    super(
+      userRepository.target,
+      userRepository.manager,
+      userRepository.queryRunner,
+    );
+  }
+
+  // Custom methods in the repo...
+}
